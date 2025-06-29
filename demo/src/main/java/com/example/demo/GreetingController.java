63df7f859db030caa717a78030c49cbfb4042739
pkoppineedi <pkoppineedi@zetaglobal.com>
2025-06-29 14:56:03 +0530

    Add message for sum calculation

--- demo/src/main/java/com/example/demo/GreetingController.java
+++ demo/src/main/java/com/example/demo/GreetingController.java
@@ -5,9 +5,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
+	
 	@GetMapping("/hi")
     public String sayHi() {
         return "Hi";
     }
+	
+	@GetMapping("/message")
+	public Integer sumOff(){
+		int a = 2;
+		int b = 4;
+		int sum = a+b;
+		return sum;
+	}
+	
+	
 
 }
